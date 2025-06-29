a7ef50a8cc706e1d3a6492160ef522ebfdb1acb1
Makvay <bojkov07@list.ru>
2025-06-29 15:44:50 +0800

    update

--- app/src/main/java/com/example/dota/HeroesDetailActivity.java
+++ app/src/main/java/com/example/dota/HeroesDetailActivity.java
@@ -1,25 +1,82 @@
 package com.example.dota;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class HeroesDetailActivity extends AppCompatActivity {
+
+    private ImageView roshaImage;
+    private Animation roshanAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_heroes_detail);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        roshaImage = findViewById(R.id.rosha_image);
+
+        // Оптимизация для плавности анимации
+        roshaImage.setLayerType(ImageView.LAYER_TYPE_HARDWARE, null);
+
+        // Инициализация анимации
+        roshanAnimation = AnimationUtils.loadAnimation(this, R.anim.animated_roshan);
+
+        // Настройка обработчика окончания анимации (опционально)
+        roshanAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                // Действия при старте анимации
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                // Автоповтор анимации
+                roshaImage.startAnimation(roshanAnimation);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+                // Действия при повторе
+            }
         });
 
+        // Запуск анимации
+        roshaImage.startAnimation(roshanAnimation);
+
+        // Обработка клика
+        roshaImage.setOnClickListener(v -> {
+            Toast.makeText(this, "Roshan активирован!", Toast.LENGTH_SHORT).show();
+
+            // Дополнительный эффект при клике
+            Animation clickAnim = AnimationUtils.loadAnimation(this, R.anim.slide_in_right);
+            roshaImage.startAnimation(clickAnim);
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (roshanAnimation != null) {
+            roshaImage.clearAnimation();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (roshanAnimation != null) {
+            roshaImage.startAnimation(roshanAnimation);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Освобождение ресурсов
+        roshanAnimation = null;
     }
 }
--- app/src/main/java/com/example/dota/MainActivity.java
+++ app/src/main/java/com/example/dota/MainActivity.java
@@ -6,13 +6,11 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -32,15 +30,33 @@ public class MainActivity extends AppCompatActivity {
         drawerLayout = findViewById(R.id.drawerLayout);
         NavigationView navView = findViewById(R.id.navView);
 
-        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+
+        toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar,
+                R.string.open, R.string.close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
+
         navView.setNavigationItemSelectedListener(item -> {
-            drawerLayout.closeDrawers();
+            int id = item.getItemId();
+
+            if (id == R.id.nav_home) {
+            } else if (id == R.id.nav_heroes) {
+                startActivity(new Intent(this, HeroesDetailActivity.class));
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+            } else if (id == R.id.nav_settings) {
+            }
+
+            drawerLayout.closeDrawer(GravityCompat.START);
             return true;
         });
 
+
+        initButtons();
+    }
+
+    private void initButtons() {
         Button btnAxe = findViewById(R.id.btnAxe);
         Button btnPudge = findViewById(R.id.btnPudge);
         Button btnAntimage = findViewById(R.id.btnAntimage);
@@ -63,29 +79,38 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if (mediaPlayer != null) mediaPlayer.release();
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+        }
     }
 
-    // Метод для создания меню тулбара (файл menu/toolbar_menu.xml должен существовать)
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.toolbar_menu, menu);
         return true;
     }
 
-    // Обработка нажатий в меню тулбара
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item) {
         if (toggle.onOptionsItemSelected(item)) {
-            return true; // для работы гамбургера Drawer
+            return true;
         }
 
         if (item.getItemId() == R.id.action_heroes_detail) {
-            Intent intent = new Intent(this, HeroesDetailActivity.class);
-            startActivity(intent);
+            startActivity(new Intent(this, HeroesDetailActivity.class));
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
